fa52571929afc525e2db5ec829b76083a848f70b
jgy2808 <jgy2808@naver.com>
2021-12-06 02:22:17 +0900

    Update CustomUserDetailsService.java

--- webappprj/src/main/java/com/jgy/webapp/security/CustomUserDetailsService.java
+++ webappprj/src/main/java/com/jgy/webapp/security/CustomUserDetailsService.java
@@ -1,17 +1,11 @@
 package com.jgy.webapp.security;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import com.jgy.webapp.jpa.UserRepository;
 import com.jgy.webapp.mapper.CustomerMapper;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
