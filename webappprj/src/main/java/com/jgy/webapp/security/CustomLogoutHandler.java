aa7b18aeb36404b706dcf624591279585329f853
jgy2808 <jgy2808@naver.com>
2021-12-08 19:36:33 +0900

    Delete not use import

--- webappprj/src/main/java/com/jgy/webapp/security/CustomLogoutHandler.java
+++ webappprj/src/main/java/com/jgy/webapp/security/CustomLogoutHandler.java
@@ -3,7 +3,6 @@ package com.jgy.webapp.security;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.naming.ServiceRef;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
